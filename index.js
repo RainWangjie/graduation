ac668df70faa45db34d7e81d477d20e75a9c6c88
gewangjie <gewangjie@RaindeMBP.lan>
2016-09-14 11:25:01 +0800

    新增

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by gewangjie on 2016/9/14.
+ */
+var express = require('express'),
+    app = express(),
+    path = require('path'),
+    server = require('http').createServer(app);
+
+
+server.listen(process.env.PORT || 3000, function () {
+    console.log('success');
+});
+
+
+app.use(express.static(path.join(__dirname, 'resources')));
+
+app.get('/', function (req, res) {
+    res.sendfile("/index.html");
+}); //指定静态HTML文件的位置
