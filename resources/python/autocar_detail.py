fefcc2423d6ee4b165879a66ddc492121bf2e45b
gewangjie <gewangjie@RaindeMBP.lan>
2016-12-15 18:15:56 +0800

    wxapp finish

--- resources/python/autocar_detail.py
+++ resources/python/autocar_detail.py
@@ -4,7 +4,11 @@ from bs4 import BeautifulSoup
 z = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M',
      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'W', 'X', 'Y', 'Z']
 
-# with open('autocar_detail.txt', 'w') as f:
+# test
+# z = ['A']
+
+car_detail = {}
+
 for word in z:
     url = 'http://www.autohome.com.cn/grade/carhtml/' + word + '.html'
     req = urllib.request.Request(url, headers={
@@ -19,18 +23,23 @@ for word in z:
     soup = BeautifulSoup(data)
     for carname in soup.find_all("dl"):
         name = carname.dt.div.a.string
+        carlist_temp = {}
         carname_itemlist = carname.find_all(class_='h3-tit')
         i = 0
         for carItem in carname.find_all(class_='rank-list-ul'):
             name_tit = carname_itemlist[i].string
             i = i + 1
+            temp_list = []
             for item in carItem.find_all('li'):
                 if item.get('class') != ['dashline']:
                     _name = item.h4.a.string
                     _price = '暂无'
-                    if item.select('.red') !=[]:
-                          _price = item.select('.red')
-                    print(name, name_tit, _name, _price)
-                # carItem = {'name': name, 'img_src': img_src}
-
-    # f.write(word + ':' + str(carlist[word]) + ',\n')
+                    if item.select('.red')!= []:
+                        _price = item.find_all(class_='red')[0].string
+                    temp_d = {'price': _price, 'name': _name}
+                    temp_list.append(temp_d)
+            carlist_temp[name_tit] = temp_list
+        car_detail[name] = carlist_temp
+    # print(car_detail)
+with open('autocar_detail.txt', 'w') as f:    
+    f.write(str(car_detail))
