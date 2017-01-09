bcead44aba1cfb97d3d35cdbbad016e58146da75
gewangjie <gewangjie@RaindeMBP.lan>
2017-01-09 18:20:57 +0800

    aaa

--- resources/vue_demo/vue_address/vue_address.js
+++ resources/vue_demo/vue_address/vue_address.js
@@ -19,50 +19,74 @@ const newAddress = Vue.component('newAddress', {
     delimiters: ['${', '}'],
     // 分隔符变成了 ES6 模板字符串的风格
     template: '#tpl_new',
-    data: () => {
+    // props: ['addressData'],//父组件传递
+    data: function () {
         return {
+            postData: {
+                name: 'Mr.Rain',
+                phone: '15757115750',
+                selectAddress: {
+                    province: citys['0'][0],
+                    city: citys['0_0'][0],
+                    district: citys['0_0_0'][0],
+                    detail: ''
+                },
+                status: 0
+            },
+            selectAddress: {
+                province: 0,
+                city: 0,
+                district: 0,
+            },
             province: citys['0'],
             city: citys['0_0'],
             district: citys['0_0_0'],
-            addressData: citys,
-            selectAddress: {
-                province: citys['0'][0],
-                city: citys['0_0'][0],
-                district: citys['0_0_0'][0],
-                detail: ""
-            },
-            select_province_temp: 0
         };
     },
+    created: function () {
+        console.log(router);
+        var id = router.query.id || '无参数id';
+    },
     methods: {
-        _print: () => {
-            console.log(this.selectAddress);
+        _print: function () {
+            console.log(this.postData.selectAddress);
         },
-        _clearCity: () => {
-            this.selectAddress.city = this.city[0];
+        _initCity: function () {
+            this.selectAddress.city = 0;
         },
-        _clearDistrict: () => {
-            this.selectAddress.district = this.district[0];
+        _initDistrict: function () {
+            this.selectAddress.district = 0;
         },
-        changeProvince: (index) => {
-            let city_index = `0_${index}`,
-                district_index = `0_${index}_0`;
-            this.select_province_temp = index;
-            this.city = this.addressData[city_index];
-            this.district = this.addressData[district_index];
-            this.selectAddress.province = this.province[index];
-            this._clearCity();
-            this._clearDistrict();
+        _changePostData: function () {
+            this.postData.selectAddress.province = this.province[this.selectAddress.province];
+            this.postData.selectAddress.city = this.city[this.selectAddress.city];
+            this.postData.selectAddress.district = this.district[this.selectAddress.district];
         },
-        changeCity: (index) => {
-            let district_index = `0_${this.select_province_temp}_${index}`;
-            this.district = this.addressData[district_index];
-            this.selectAddress.city = this.city[index];
-            this._clearDistrict();
+        changeProvince: function () {
+            var temp_city = `0_${this.selectAddress.province}`,
+                temp_district = `0_${this.selectAddress.province}_${this.selectAddress.city}`;
+            this.city = citys[temp_city];
+            this.district = citys[temp_district];
+            this._initCity();
+            this._initDistrict();
+            this._changePostData();
+            this._print();
         },
-        changeDistrict: (index) => {
-            this.selectAddress.district = this.district[index];
+        changeCity: function () {
+            var temp_district = `0_${this.selectAddress.province}_${this.selectAddress.city}`;
+            this.district = citys[temp_district];
+            this._initDistrict();
+            this._changePostData();
+            this._print();
+        },
+        changeDistrict: function () {
+            this._changePostData();
+            this._print();
+        },
+        postData: function () {
+
         }
+
     }
 });
 const mangerAddress = Vue.component('mangerAddress', {
@@ -73,6 +97,27 @@ const mangerAddress = Vue.component('mangerAddress', {
         return {
             addressList: data_temp
         };
+    },
+    methods: {
+        editAddress: function (event, id) {
+            // 跳转到新增地址页面
+            console.log(`跳转到Id为${id}的地址编辑页`);
+            router.go({path: `/newAddress?id=${id}`});
+        },
+        deleteAddress: function (event, id) {
+            alert(`删除Id为${id}的地址`);
+        },
+        setStatus: function (event, id) {
+            if (this.addressList[id].id == id) {
+                for (var i in this.addressList) {
+                    this.addressList[i].status = 0;
+                }
+                this.addressList[id].status = 1;
+            } else {
+                alert('数据错误')
+            }
+
+        }
     }
 });
 // 2. 定义路由
@@ -89,14 +134,14 @@ const routes = [
 // 3. 创建 router 实例，然后传 `routes` 配置
 // 你还可以传别的配置参数, 不过先这么简单着吧。
 const router = new VueRouter({
-    routes // （缩写）相当于 routes: routes
+    routes// （缩写）相当于 routes: routes
 });
 
 // 4. 创建和挂载根实例。
 // 记得要通过 router 配置参数注入路由，
 // 从而让整个应用都有路由功能
 const app = new Vue({
-    router
+    router: router,
 }).$mount('#app');
 
 // 现在，应用已经启动了！
