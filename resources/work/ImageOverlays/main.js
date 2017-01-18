eeb76a73790036251940d04a05383faf210c3977
gewangjie <gewangjie@RaindeMBP.lan>
2017-01-18 21:36:29 +0800

    image

---
+++ resources/work/ImageOverlays/main.js
@@ -0,0 +1,113 @@
+/**
+ * Created by gewangjie on 2017/1/18.
+ */
+var labelData = {
+    'shoes': {
+        'name': '鞋子',
+        'color': 'red'
+    },
+    'pants': {
+        'name': '裤子',
+        'color': 'blue'
+    },
+    'Jacket': {
+        'name': '上衣',
+        'color': 'green'
+    }
+};
+function initLabel() {
+    var html = '';
+    for (var i in labelData) {
+        html += '<li>' +
+            '<input type="checkbox" class="imgLabel" name="imgLabel" value="' + i + '">' + labelData[i].name +
+            '<div class="color-block" style="background: ' + labelData[i].color + '"></div>' +
+            '</li>';
+    }
+    $('.label-list').append(html);
+}
+initLabel();
+
+var isMouseDown = false,
+    mouse = captureMouse(document.getElementById('img-self')),
+    newLabel = {},
+    selectLabel = {},
+    labelList = [],
+    labelTotal = 0,
+    labelMove = {};
+
+function clearNewLabel() {
+    newLabel = {};
+}
+// 创建label
+$('#img-area').mousedown(function (event) {
+    isMouseDown = true;
+    newLabel.x = mouse.x;
+    newLabel.y = mouse.y;
+    newLabel.el = $($('#tpl-area').html());
+    newLabel.isExist = false;
+    console.log('img');
+    return false;
+}).mousemove(function (event) {
+    var difference_x = mouse.x - newLabel.x,
+        difference_y = mouse.y - newLabel.y;
+    // 鼠标点击，操作距离大于20px
+    if (isMouseDown) {
+        if (!newLabel.isExist) {
+            newLabel.el.attr('id', 'label_' + (labelTotal++));
+            $(this).append(newLabel.el);
+            newLabel.isExist = true;
+        }
+        if (difference_x > 0 && difference_y > 0) {
+            newLabel.el.css({
+                top: newLabel.y,
+                left: newLabel.x,
+                width: Math.max(difference_x, 20),
+                height: Math.max(difference_y, 20)
+            })
+        } else {
+            console.log('勿反向操作!')
+        }
+    }
+    return false;
+}).mouseup(function () {
+    isMouseDown = false;
+    newLabel.width = Math.abs(mouse.x - newLabel.x);
+    newLabel.height = Math.abs(mouse.y - newLabel.y);
+    labelList.push(newLabel);
+    clearNewLabel();
+    console.log('mouseup');
+    return false;
+}).mouseleave(function (event) {
+    if (isMouseDown) {
+        isMouseDown = false;
+        newLabel.width = Math.abs(mouse.x - newLabel.x);
+        newLabel.height = Math.abs(mouse.y - newLabel.y);
+        labelList.push(newLabel);
+        clearNewLabel();
+        console.log('mouseleave');
+    }
+    return false;
+});
+
+// 移动area
+$('.label-area').mousedown(function (event) {
+    isMouseDown = true;
+    labelMove = {
+        x: mouse.x,
+        y: mouse.y
+    };
+    console.log('label');
+    return false;
+}).mousemove(function (event) {
+    if (isMouseDown) {
+        var id = $(this).attr('id').replace('label_', '');
+        $(this).css({
+            top: labelList[id].y + (labelMove.y - mouse.y),
+            left: labelList[id].x + (labelMove.x - mouse.x),
+        })
+    }
+    return false;
+}).mouseup(function (event) {
+    isMouseDown = false;
+    return false;
+});
